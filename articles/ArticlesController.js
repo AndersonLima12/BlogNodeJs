069f76a7b593b9acc56908e33e4873c5f39229b4
Anderson Lima <anderson_developer@outlook.com>
2022-10-27 10:01:40 -0300

    feat: adding delete and edit.

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -33,4 +33,57 @@ router.post("/articles/save", (req, res) => {
   });
 });
 
+router.post("/articles/delete", (req, res) => {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/articles");
+      });
+    } else {
+      // NÃO FOR UM NÚMERO
+      res.redirect("/admin/articles");
+    }
+  } else {
+    // NULL
+    res.redirect("/admin/articles");
+  }
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+  // Metodo de pesquisa pelo id do artigo = findByPk
+  let id = req.params.id;
+  Article.findByPk(id)
+    .then((article) => {
+      if (article != undefined) {
+        res.render("admin/articles/edit", { article: article });
+      } else {
+        res.redirect("/admin/articles");
+      }
+    })
+    .catch((erro) => {
+      res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/update", (req, res) => {
+  let id = req.body.id;
+  let title = req.body.title;
+
+  Article.update(
+    { title: title, slug: slugify(title) },
+    {
+      where: {
+        id: id,
+      },
+    }
+  ).then(() => {
+    res.redirect("/admin/articles");
+  });
+});
+
 module.exports = router;
